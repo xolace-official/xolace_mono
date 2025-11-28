ded48ba0f455bb9ad6e3c44bc63f19f3a2f6bba0
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-11-28 20:30:46 +0000

    chore/change color syntax

--- tooling/tailwind-config/index.js
+++ tooling/tailwind-config/index.js
@@ -12,7 +12,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "hsl(var(--color-primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
